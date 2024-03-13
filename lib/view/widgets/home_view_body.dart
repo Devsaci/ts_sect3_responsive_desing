b77d9dccc67bb6a68255af6144b8acb024ff8cb5
saci zakaria <sacideveloppeur@gmail.com>
2024-03-13 11:14:48 +0100

    SliverGrid.builder

--- lib/view/widgets/home_view_body.dart
+++ lib/view/widgets/home_view_body.dart
@@ -5,6 +5,29 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(slivers: []);
+    return const CustomScrollView(
+      slivers: [
+        CustomsSliverGrid(),
+      ],
+    );
+  }
+}
+
+class CustomsSliverGrid extends StatelessWidget {
+  const CustomsSliverGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          color: Colors.red,
+        );
+      },
+      itemCount: 4,
+    );
   }
 }
