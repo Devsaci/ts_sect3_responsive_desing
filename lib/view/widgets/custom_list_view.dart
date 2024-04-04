36ca6cafb5fcd97af6a8e1d43ab8e0a866d57fe0
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 21:46:13 +0200

    class CustomSliverListView extends StatefulWidget

---
+++ lib/view/widgets/custom_list_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomSliverListView extends StatefulWidget {
+  const CustomSliverListView({super.key});
+
+  @override
+  State<CustomSliverListView> createState() => _CustomSliverListViewState();
+}
+
+class _CustomSliverListViewState extends State<CustomSliverListView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
