b777982ea6e8e4251576581e5436d6ec40c193d6
saci zakaria <sacideveloppeur@gmail.com>
2024-04-04 21:50:17 +0200

    liverList.builder

--- lib/view/widgets/custom_list_view.dart
+++ lib/view/widgets/custom_list_view.dart
@@ -10,6 +10,18 @@ class CustomSliverListView extends StatefulWidget {
 class _CustomSliverListViewState extends State<CustomSliverListView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SliverList.builder(itemBuilder: (BuildContext context, int index) {
+      return CustomItem2();
+    });
+  }
+}
+
+class CustomItem2 extends StatelessWidget {
+  const CustomItem2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
