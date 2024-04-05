95e3e019d998a73a6cd3bbf285a4e180b871a457
saci zakaria <sacideveloppeur@gmail.com>
2024-04-05 09:12:25 +0200

    format code

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -8,16 +8,49 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        drawer: const CostumDrawer(),
         backgroundColor: const Color.fromARGB(250, 248, 189, 25),
         appBar: AppBar(
           backgroundColor: const Color.fromRGBO(59, 54, 107, 42),
-          leading:
-              const Icon(Icons.menu, color: Color.fromRGBO(235, 229, 0, 92)),
-          title: const Text('Home',
-              style: TextStyle(color: Color.fromRGBO(235, 229, 0, 92))),
+          leading: const Icon(
+            Icons.menu,
+            color: Color.fromRGBO(235, 229, 0, 92),
+          ),
+          title: const Text(
+            'Home',
+            style: TextStyle(
+              color: Color.fromRGBO(235, 229, 0, 92),
+            ),
+          ),
           centerTitle: true,
         ),
         body: const HomeViewBody(),
+      ),
+    );
+  }
+}
+
+class CostumDrawer extends StatelessWidget {
+  const CostumDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: const [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(241, 178, 5, 0.983),
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: Text('Item 1'),
+          ),
+          ListTile(
+            title: Text('Item 2'),
+          ),
+        ],
       ),
     );
   }
