28f2fa4374e9ab4c45cae8ff97d9462b5d4cbb03
saci zakaria <sacideveloppeur@gmail.com>
2024-04-16 21:53:26 +0200

    CustomScrollView

--- lib/view/widgets/tablet_layout.dart
+++ lib/view/widgets/tablet_layout.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'custom_list_view.dart';
+import 'customs_sliver_grid.dart';
+
 class TabletLayout extends StatelessWidget {
   const TabletLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: SizedBox(height: 15)),
+        CustomsSliverGrid(),
+        SliverToBoxAdapter(child: SizedBox(height: 15)),
+        CustomSliverListView(),
+      ],
+    );
   }
 }
