6d87abdafe7f945ec825ee2635ab9ee3f7b9c001
saci zakaria <sacideveloppeur@gmail.com>
2024-04-16 21:38:37 +0200

    Extract widget MobileLayout

--- lib/view/widgets/home_view_body.dart
+++ lib/view/widgets/home_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ts_sect3_responsive_desing/view/widgets/custom_list.dart';
 
 import 'custom_list_view.dart';
 import 'customs_sliver_grid.dart';
@@ -13,18 +12,29 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 15),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          CustomsSliverGrid(),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 15),
-          ),
-          CustomSliverListView(),
-        ],
-      ),
+      child: MobileLayout(),
+    );
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SizedBox(height: 15),
+        ),
+        CustomsSliverGrid(),
+        SliverToBoxAdapter(
+          child: SizedBox(height: 15),
+        ),
+        CustomSliverListView(),
+      ],
     );
   }
 }
