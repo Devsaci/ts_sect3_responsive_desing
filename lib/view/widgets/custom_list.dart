2290fcea366c7e7154df73cd23d1aab2bba701f0
saci zakaria <sacideveloppeur@gmail.com>
2024-05-09 22:52:43 +0200

    height: 160,

--- lib/view/widgets/custom_list.dart
+++ lib/view/widgets/custom_list.dart
@@ -9,7 +9,7 @@ class CustomList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverToBoxAdapter(
       child: SizedBox(
-        height: 100,
+        height: 160,
         child: ListView.builder(
           itemCount: 15,
           scrollDirection: Axis.horizontal,
